dd8aea129f15767d80d73cf98bc437cdc45edcd1
abhishek <abhishek.ravula@gsynergy.com>
2022-04-16 16:21:58 +0530

    removed spotify folder in src

--- src/components/LikedMusic.js
+++ src/components/LikedMusic.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "../styles//PlaylistView.css";
+import "../styles/PlaylistView.css";
 import Song from "./SongView";
 import { HOSTNAME } from "../spotify.constants.js";
 
